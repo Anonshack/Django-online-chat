e82970858e5d112e8c3285a31b7cda604e701964
Anonshack <anonshack48@gmail.com>
2024-10-08 15:35:53 +0500

    test

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -64,8 +64,11 @@ ASGI_APPLICATION = 'djangochat.asgi.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer'
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
 }
 
 DATABASES = {
