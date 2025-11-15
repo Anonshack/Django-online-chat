cc3c7df65082b4880ced873cb046f5f031edef98
Кудратбек Казакжонов <140953705+Anonshack@users.noreply.github.com>
2025-11-15 12:12:21 +0500

    Delete salo.py

--- salo.py
+++
@@ -1,9 +0,0 @@
-clay = int(input())
-
-for i in range(clay):
-    for j in range(clay - i - 1):
-        print(" ", end=" ")
-    for k in range(i * 2 + 1):
-        print("*", end=" ")
-    print()
-
